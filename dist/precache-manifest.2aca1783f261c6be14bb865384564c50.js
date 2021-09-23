34d8772fabb89da74ff98e3d11f8c5eef528d683
강지혜 <z000000281@wemakeprice.com>
2021-09-23 18:24:41 +0900

    initial commit for dist folder

---
+++ dist/precache-manifest.2aca1783f261c6be14bb865384564c50.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82315d0b88897319e3df",
+    "url": "/jhkang-portfolio/css/app.4d43d97f.css"
+  },
+  {
+    "revision": "3055358b4923ceaa58f4",
+    "url": "/jhkang-portfolio/css/projects.7c06c07e.css"
+  },
+  {
+    "revision": "412f7cfd23005c797706640a87e654ac",
+    "url": "/jhkang-portfolio/images/devloper_banner.412f7cfd.svg"
+  },
+  {
+    "revision": "ae57da8f2747b2f7b564f3e2bfbfd08f",
+    "url": "/jhkang-portfolio/images/devloper_banner_dark.ae57da8f.svg"
+  },
+  {
+    "revision": "71b2d5f6bb9263c0cfc3c422949c4405",
+    "url": "/jhkang-portfolio/images/header_listIcon.71b2d5f6.svg"
+  },
+  {
+    "revision": "827ae31c8433cce76006ecdc4ffd9f31",
+    "url": "/jhkang-portfolio/img/project_200909_01.827ae31c.jpg"
+  },
+  {
+    "revision": "8416bc269fc0b8e856e36462143326be",
+    "url": "/jhkang-portfolio/img/project_200909_01_main.8416bc26.jpg"
+  },
+  {
+    "revision": "9e8ae13ef3c11842a1a4a5ccffa616b1",
+    "url": "/jhkang-portfolio/img/project_200909_02.9e8ae13e.jpg"
+  },
+  {
+    "revision": "a772deb48783394058d4c3e565a5f984",
+    "url": "/jhkang-portfolio/img/project_200909_03.a772deb4.jpg"
+  },
+  {
+    "revision": "ca18da5d3359e9cd0879fc3a5daa068d",
+    "url": "/jhkang-portfolio/img/project_200910_01.ca18da5d.jpg"
+  },
+  {
+    "revision": "9d1d5edfb2ebee729027e0eaab0ccbef",
+    "url": "/jhkang-portfolio/img/project_200910_01_main.9d1d5edf.jpg"
+  },
+  {
+    "revision": "f33c870d378e78d7c4585a06df327d36",
+    "url": "/jhkang-portfolio/img/project_200910_02.f33c870d.jpg"
+  },
+  {
+    "revision": "2a9cc111c11c593a05ab339bacc138c8",
+    "url": "/jhkang-portfolio/img/project_200910_03.2a9cc111.jpg"
+  },
+  {
+    "revision": "ae9cc32ea4d013860adbc315e1320700",
+    "url": "/jhkang-portfolio/img/project_200910_04.ae9cc32e.jpg"
+  },
+  {
+    "revision": "0230d06d5d67375f5dd03d97182d2b2c",
+    "url": "/jhkang-portfolio/img/project_2010_01.0230d06d.jpg"
+  },
+  {
+    "revision": "256a2ad61b9de3db62407388699bf128",
+    "url": "/jhkang-portfolio/img/project_2010_01_main.256a2ad6.jpg"
+  },
+  {
+    "revision": "5ba2ddcccc82cae241d5ca8dc5d30208",
+    "url": "/jhkang-portfolio/img/project_2010_02.5ba2ddcc.jpg"
+  },
+  {
+    "revision": "0dda40d8306239fa41403d764eb973e3",
+    "url": "/jhkang-portfolio/img/project_2015_01.0dda40d8.png"
+  },
+  {
+    "revision": "0cf81ca55f7fad3ebe065fbf7daf6bae",
+    "url": "/jhkang-portfolio/img/project_2015_01_main.0cf81ca5.jpg"
+  },
+  {
+    "revision": "fd2cfdf310d88ce881e06dcbd603c09e",
+    "url": "/jhkang-portfolio/img/project_2015_02.fd2cfdf3.png"
+  },
+  {
+    "revision": "49948ed43c19198d860201329e968400",
+    "url": "/jhkang-portfolio/img/project_2015_03.49948ed4.png"
+  },
+  {
+    "revision": "4489e891bec0d3b5e7a765d0f831fadc",
+    "url": "/jhkang-portfolio/img/project_202105_01.4489e891.png"
+  },
+  {
+    "revision": "083e9d3dd2bcbc91d82510803532361e",
+    "url": "/jhkang-portfolio/img/project_202105_01_main.083e9d3d.jpg"
+  },
+  {
+    "revision": "1a4294df2adac9f93038b009e7659766",
+    "url": "/jhkang-portfolio/img/project_202105_02.1a4294df.png"
+  },
+  {
+    "revision": "242f5f5ed094dfda29662cce88a3e4bd",
+    "url": "/jhkang-portfolio/img/project_202107_01.242f5f5e.jpg"
+  },
+  {
+    "revision": "3811825d4bceff5ed3f242c11de3315c",
+    "url": "/jhkang-portfolio/img/project_202107_02.3811825d.jpg"
+  },
+  {
+    "revision": "fa2ce9aec84f808bf7b55821850c90bd",
+    "url": "/jhkang-portfolio/img/project_202107_03.fa2ce9ae.jpg"
+  },
+  {
+    "revision": "b2561000d4e081299fcb118d8b15c046",
+    "url": "/jhkang-portfolio/img/project_202107_04.b2561000.jpg"
+  },
+  {
+    "revision": "2a1e9227bed438294e4979b226b0de1f",
+    "url": "/jhkang-portfolio/img/project_202107_05.2a1e9227.jpg"
+  },
+  {
+    "revision": "d693921e313285ec9afdcec68da7b3e7",
+    "url": "/jhkang-portfolio/img/project_2021_07_main.d693921e.jpg"
+  },
+  {
+    "revision": "493619f018862c4cc61d1b1c013f71b9",
+    "url": "/jhkang-portfolio/index.html"
+  },
+  {
+    "revision": "7ce28aaa169b3440daa2",
+    "url": "/jhkang-portfolio/js/about.7d6a125a.js"
+  },
+  {
+    "revision": "82315d0b88897319e3df",
+    "url": "/jhkang-portfolio/js/app.7d6a125a.js"
+  },
+  {
+    "revision": "374f138171443e4156e1",
+    "url": "/jhkang-portfolio/js/chunk-vendors.7d6a125a.js"
+  },
+  {
+    "revision": "3055358b4923ceaa58f4",
+    "url": "/jhkang-portfolio/js/projects.7d6a125a.js"
+  },
+  {
+    "revision": "733e037baaa60059a409132a0a3cf5e4",
+    "url": "/jhkang-portfolio/manifest.json"
+  }
+]);
